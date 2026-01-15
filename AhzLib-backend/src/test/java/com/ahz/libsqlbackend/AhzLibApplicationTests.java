e091e5c3dda70da8f48e798cb77590128cda92f1
ahz <abby09302005@gmail.com>
2026-01-15 22:02:36 +0800

    fix: 修复测试类可见性问题

--- AhzLib-backend/src/test/java/com/ahz/libsqlbackend/AhzLibApplicationTests.java
+++ AhzLib-backend/src/test/java/com/ahz/libsqlbackend/AhzLibApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class AhzLibApplicationTests {
+public class AhzLibApplicationTests {
 
     @Test
     void contextLoads() {
